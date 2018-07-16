338f6fc395c28aceaed8d24d935ab789ec751a11
FRex <rex2k8@gmail.com>
2018-07-16 15:44:54 +0200

    small chaneg

--- src/checkCudaCall.hpp
+++ src/checkCudaCall.hpp
@@ -1,11 +1,15 @@
 #pragma once
 #include <cstdio>
 #include "cuda_runtime_api.h"
+#include <cstdlib>
 
 inline void impl_checkCudaCall(cudaError_t ret, const char * file, int line)
 {
     if(ret != cudaSuccess)
+    {
         std::fprintf(stderr, "%s:%d: cuda error %d\n", file, line, static_cast<int>(ret));
+        std::exit(1);
+    }
 }
 
 #define checkCudaCall(r) impl_checkCudaCall(r, __FILE__, __LINE__)
