0a7103a39833d60f7b344da3b06708afac7e89ba
FRex <rex2k8@gmail.com>
2018-07-23 01:04:57 +0200

    gpu only transfer from cuda via pbo to texture

--- src/transferViaPBO.cpp
+++ src/transferViaPBO.cpp
@@ -2,32 +2,35 @@
 #include "gl_core21.h"
 #include <SFML/Graphics/Texture.hpp>
 #include <cstring>
+#include "checkCudaCall.hpp"
+#include "cuda_gl_interop.h"
 
 void transferViaPBO(unsigned * cudascreen, sf::Texture& texture, unsigned pbo)
 {
-    sf::Texture::bind(&texture);
-    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
     const int x = static_cast<int>(texture.getSize().x);
     const int y = static_cast<int>(texture.getSize().y);
     const unsigned size = texture.getSize().x * texture.getSize().y * 4;
-    auto ptr = (unsigned char*)glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
-    if(ptr)
-    {
-        std::memset(ptr, 0xff, size);
-        for(int i = 0; i < size; i += 4)
-            ptr[i + 0] = 0x0;
 
-        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
-        sf::Texture::bind(&texture);
-        glTexImage2D(
-            GL_TEXTURE_2D,
-            0,
-            GL_RGBA,
-            x, y, 0,
-            GL_BGRA,
-            GL_UNSIGNED_BYTE,
-            0x0
-        );
-    }
+    cudaGraphicsResource * res;
+    checkCudaCall(cudaGraphicsGLRegisterBuffer(&res, pbo, cudaGraphicsRegisterFlagsWriteDiscard));
+    void * ptr = 0x0;
+    size_t buffsize = 0u;
+    checkCudaCall(cudaGraphicsMapResources(1, &res));
+    checkCudaCall(cudaGraphicsResourceGetMappedPointer(&ptr, &buffsize, res));
+    checkCudaCall(cudaMemcpy(ptr, cudascreen, buffsize, cudaMemcpyDeviceToDevice));
+    checkCudaCall(cudaGraphicsUnmapResources(1, &res));
+    checkCudaCall(cudaGraphicsUnregisterResource(res));
+
+    sf::Texture::bind(&texture);
+    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RGBA,
+        x, y, 0,
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        0x0
+    );
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 }
